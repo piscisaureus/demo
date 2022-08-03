a3838e15c05409821219ab5de8fcf5ef23eece35
Bert Belder <bertbelder@gmail.com>
2022-08-03 13:52:41 -0700

    next

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
 
 function handler(req: Request): Response {
-  return new Response("Hello world");
+  return new Response("Hello moon!");
 }
 
 console.log("Listening on http://localhost:8000");
